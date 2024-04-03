a077db64b62161e22cf0bc044a570e01545a8c8c
JoelDoitlean <joel.simoes@doitlean.com>
2024-04-03 16:51:36 +0100

    Update OSCamera.swift

--- src/ios/OSCamera.swift
+++ src/ios/OSCamera.swift
@@ -20,8 +20,7 @@ class OSCamera: CDVPlugin {
     @objc(takePicture:)
     func takePicture(command: CDVInvokedUrlCommand) {
         self.callbackId = command.callbackId
-    let defaultLog = Logger()
-   defaultLog.log("This is a default message.")
+    os_log("This is a default message.")
         guard let parametersDictionary = command.argument(at: 0) as? [String: Any],
               let parametersData = try? JSONSerialization.data(withJSONObject: parametersDictionary),
               let parameters = try? JSONDecoder().decode(OSCAMRTakePictureParameters.self, from: parametersData)
