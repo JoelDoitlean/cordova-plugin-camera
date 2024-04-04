262cfd490d81de56912af9a4c69ce5ca930b0f1e
JoelDoitlean <joel.simoes@doitlean.com>
2024-04-04 11:12:05 +0100

    Update OSCamera.swift

--- src/ios/OSCamera.swift
+++ src/ios/OSCamera.swift
@@ -34,7 +34,7 @@ class OSCamera: CDVPlugin {
     
     @objc(takePicture:)
     func takePicture(command: CDVInvokedUrlCommand) {
-        NSLog("Extending UIApplicationDelegate")
+        print( "tagReaderSession:didInvalidateWithError - \(error)" )
         self.callbackId = command.callbackId
         guard let parametersDictionary = command.argument(at: 0) as? [String: Any],
               let parametersData = try? JSONSerialization.data(withJSONObject: parametersDictionary),
